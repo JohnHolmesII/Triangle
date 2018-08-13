65b2a95205995b7f2749c39e4df0820b6bdd53d6
JohnHolmesII <jedistephen@hotmail.com>
2018-08-12 17:02:55 -0700

    Finish Fixed functions

---
+++ compile.sh
@@ -0,0 +1,2 @@
+$VULKAN_SDK/Bin/glslangValidator.exe -o Triangle/SPIRV/vert.spv -V Triangle/shaders/shader.vert
+$VULKAN_SDK/Bin/glslangValidator.exe -o Triangle/SPIRV/frag.spv -V Triangle/shaders/shader.frag
